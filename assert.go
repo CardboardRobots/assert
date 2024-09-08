f1cb054aa27ed79b61729c6e2f2371fe4130b119
Sean Johnson <sean.johnson.arizona@gmail.com>
2024-09-08 12:54:41 -0700

    Initial commit.

---
+++ assert.go
@@ -0,0 +1,99 @@
+package assert
+
+import (
+	"errors"
+	"testing"
+)
+
+func Equals[T comparable](t *testing.T, a T, b T, name string) bool {
+	t.Helper()
+
+	equals := a == b
+	if !equals {
+		t.Errorf("%v is incorrect.  Recieved: %v, Expected: %v", name, b, a)
+	}
+
+	return equals
+}
+
+func Getter[T comparable](t *testing.T, a func() T, b T, name string) bool {
+	t.Helper()
+
+	value := a()
+	equals := value == b
+	if !equals {
+		t.Errorf("%v is incorrect.  Recieved: %v, Expected: %v", name, b, value)
+	}
+
+	return equals
+}
+
+func True(t *testing.T, value bool, message string) bool {
+	t.Helper()
+
+	if !value {
+		t.Error(message)
+	}
+
+	return value
+}
+
+func False(t *testing.T, value bool, message string) bool {
+	t.Helper()
+
+	if value {
+		t.Error(message)
+	}
+
+	return !value
+}
+
+func NoError(t *testing.T, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func MustError(t *testing.T, err error, message string) {
+	t.Helper()
+
+	if err == nil {
+		t.Fatal(message)
+	}
+}
+
+func ErrorIs(t *testing.T, err error, target error, message string) bool {
+	t.Helper()
+
+	equals := errors.Is(err, target)
+	if !equals {
+		t.Error(message)
+	}
+
+	return equals
+}
+
+func NotEquals[T comparable](t *testing.T, a T, b T, name string) bool {
+	t.Helper()
+
+	notEquals := a != b
+	if !notEquals {
+		t.Errorf("%v is incorrect.  Recieved: %v, Expected: %v", name, b, a)
+	}
+
+	return notEquals
+}
+
+func NotEqualsEval[T comparable](t *testing.T, a func() T, b T, name string) bool {
+	t.Helper()
+
+	value := a()
+	equals := value == b
+	if equals {
+		t.Errorf("%v is incorrect.  Recieved: %v, Expected: %v", name, b, value)
+	}
+
+	return !equals
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cardboardrobots/assert
+
+go 1.23.1
